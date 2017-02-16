0311c313e47796c47424432cd577be3f2ad5a74a
Alex Pedersen <karen@Alexs-MacBook.local>
2017-02-15 22:10:48 -0500

    avg good

--- speed.py
+++ speed.py
@@ -23,7 +23,7 @@ firstFrame = None
 car_in_frame = False
 frame_count = 0
 frames_to_pass = []
-frames_list = []
+
 prev_frame = None
 
 while True:
@@ -31,44 +31,40 @@ while True:
     # text
     (grabbed, frame) = camera.read()
     # if the frame could not be grabbed, then we have reached the end
-        # of the video
-        #if not grabbed:
-         #   break
+    # of the video
+    #if not grabbed:
+     #   break
 
-        # resize the frame, convert it to grayscale, and blur it
-        #frame = imutils.resize(frame, width=500)
+    # resize the frame, convert it to grayscale, and blur it
+    #frame = imutils.resize(frame, width=500)
 
     if prev_frame is None:
         prev_frame = frame
         continue
 
-        # compute the absolute difference between the current frame and
-        # first frame
+    # compute the absolute difference between the current frame and
+    # first frame
     frameDelta = cv2.absdiff(prev_frame, frame)
     frameDeltaAvg = np.average(frameDelta)
-    if len(frames_list) > 10:
-        variation = np.std(frames_list)/np.average(frames_list)
-        #1.4
-        if car_in_frame and variation > .1:
-            frame_count = frame_count + 1
-        elif not car_in_frame and variation > .1:
-            car_in_frame = True
-            frame_count = 1
-        elif car_in_frame:
-            if frame_count > 10:
-                print(frame_count)
-                print(width)
-                print((frame_count/30.0))
-                meters_per_second = width/(frame_count/30.0)
-                miles_per_hour = ((meters_per_second*360000)/160934.4)
-                #if miles_per_hour > 20:
+    
+    if car_in_frame and frameDeltaAvg > 1.4:
+        frame_count = frame_count + 1
+    elif not car_in_frame and frameDeltaAvg > 1.4:
+        car_in_frame = True
+        frame_count = 1
+    elif car_in_frame:
+        if frame_count > 10:
+            print(frame_count)
+            print(width)
+            print((frame_count/30.0))
+            meters_per_second = width/(frame_count/30.0)
+            miles_per_hour = ((meters_per_second*360000)/160934.4)
+            if miles_per_hour > 30:
                 print(str(meters_per_second) + "m/s")
                 print(str(miles_per_hour) + "mph")
                 frames_to_pass.append(frame_count)
+            
+        car_in_frame = False
+        frame_count = 0
 
-            car_in_frame = False
-            frame_count = 0
-        frames_list.pop()
-        prev_frame = frame
-        
-    frames_list.insert(0,frameDeltaAvg)
+    prev_frame = frame
